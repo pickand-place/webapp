b7c59cc33916057200b0a8b847d8b11dfa4709f2
Carl St-Laurent <carl@carlstlaurent.com>
2021-02-27 10:53:20 -0500

    Use router

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import "windi.css"
 
 import { createApp } from "vue"
+import { createRouter, createWebHashHistory } from "vue-router"
 import App from "./App.vue"
 
-createApp(App).mount("#app")
+const router = createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHashHistory(),
+  routes: [], // short for `routes: routes`
+})
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount("#app")
